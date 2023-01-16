35efbe4d161d9fa3858fb75a0d7f7bd930201f45
zhanglin233 <1257730824@qq.com>
2023-01-16 21:23:15 +0800

    gin 路由测试

---
+++ router/router.go
@@ -0,0 +1,13 @@
+package router
+
+import (
+	"chatgpt/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Router() *gin.Engine {
+	r := gin.Default()
+	r.GET("/index", service.GetIndex)
+	return r
+}
