e11c8885490813e27ab2b067b053d06562b46e9e
zhanglin233 <1257730824@qq.com>
2023-01-16 21:23:00 +0800

    gorm  mysql 连通性测试

---
+++ models/UserBasic.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type UserBasic struct {
+	gorm.Model
+	Name          string
+	Password      string
+	Phone         string
+	Email         string
+	Identity      string
+	ClientIp      string
+	ClientPort    string
+	LoginTime     uint64
+	HeartbeatTime uint64
+	LogoutTIME    uint64
+	IsLogout      bool
+	DeviceInfo    string
+}
+
+func (table *UserBasic) TableName() string {
+	return "user_basic"
+}
---
+++ test/testGorm.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"chatgpt/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open(mysql.Open("root:zl29010129@tcp(127.0.0.1:3306)/ginchat?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// 迁移 schema
+	db.AutoMigrate(&models.UserBasic{})
+
+	user := &models.UserBasic{}
+	user.Name = "zhanglin"
+	// Create
+	db.Create(user)
+
+	// Read
+	// var product Product
+	fmt.Println(db.First(user, 1)) // 根据整型主键查找
+	// db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+
+	// Update - 将 product 的 price 更新为 200
+	db.Model(user).Update("Password", "1234")
+	// Update - 更新多个字段
+	// db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
+	// db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - 删除 product
+	// db.Delete(&product, 1)
+}
